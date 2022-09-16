7e3b7c5d63e6b907b39dc2c7f5766ed41c2ae6d9
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2022-09-16 14:39:51 -0300

    chore(header): adicionado componente header

--- 2022/projeto1/01-fundamentos-reactjs/src/App.jsx
+++ 2022/projeto1/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+import './styles.css'
 
 export function App() {
   return (
     <>
+    <Header />
       <Post
         autor="Guilherme Jordão"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae corrupti vero porro error expedita nihil veniam praesentium quibusdam iusto a! Et minus quo autem recusandae veritatis vero dicta ducimus rerum." 
