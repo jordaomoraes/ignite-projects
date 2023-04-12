482edf2892c338d1557e6191cb7325bc6eda3980
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2023-04-12 17:24:34 -0300

    chore(rotas): adicionando novas rotas e configuraçoes

--- 2023/nodeJs/projeto-02/src/server.ts
+++ 2023/nodeJs/projeto-02/src/server.ts
@@ -1,20 +1,11 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Teste de transação',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
