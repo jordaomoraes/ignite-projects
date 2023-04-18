8382aaed7968e7e086645d53ec46d3105d550d05
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2023-04-18 15:00:38 -0300

    chore: configurando eslint

--- 2023/nodeJs/projeto-03/src/env/index.ts
+++ 2023/nodeJs/projeto-03/src/env/index.ts
@@ -1,18 +1,17 @@
-import 'dotenv/config' //aqui importa as variaveis de ambiante
-import { z } from 'zod' // usado para validar as variaveis de ambiente
+import "dotenv/config"; // aqui importa as variaveis de ambiante
+import { z } from "zod"; // usado para validar as variaveis de ambiente
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333) // Converte qualquer valor pra numero
-})
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(3333), // Converte qualquer valor pra numero
+});
 
-const _env = envSchema.safeParse(process.env)  // aqui valida pra ser se exstem as informaçoes
+const _env = envSchema.safeParse(process.env); // aqui valida pra ser se exstem as informaçoes
 
-if(_env.success === false) {
+if (_env.success === false) {
+  console.error("Invalid environment variables", _env.error.format()); // pega todos os erros e formata de forma amigavel
 
-  console.error('Invalid environment variables', _env.error.format()) //pega todos os erros e formata de forma amigavel
-
-  throw new Error('Invalid environment variables.') //aqui já derruba antes a aplicação
+  throw new Error("Invalid environment variables."); // aqui já derruba antes a aplicação
 }
 
-export const env = _env.data
+export const env = _env.data;
