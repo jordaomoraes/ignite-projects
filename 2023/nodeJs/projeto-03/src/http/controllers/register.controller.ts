d4168c0496ff3288df1660f59ca7f09f808aac5e
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2023-04-25 15:17:18 -0300

    feat: adicionado use cases

--- 2023/nodeJs/projeto-03/src/http/controllers/register.controller.ts
+++ 2023/nodeJs/projeto-03/src/http/controllers/register.controller.ts
@@ -1,7 +1,6 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { hash } from "bcryptjs";
-import { prisma } from "@/lib/prisma";
 import { z } from "zod";
+import { registerUseCase } from "@/use-cases/register";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,15 +11,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const password_hash = await hash(password, 6); // 6 e o parametro para gerar o hash, zerar 6 rounds de complexidaded
-
-  await prisma.user.create({
-    data: {
+  try {
+    await registerUseCase({
       name,
       email,
-      password_hash,
-    },
-  });
+      password,
+    });
+  } catch (err) {
+    return reply.status(409).send();
+  }
 
   return reply.status(201).send();
 }
