ca4dfcd9b7e27ae076fb3990db2a6bf2ae55b251
Guilherme Jordão <guijmoraes@hotmail.com>
2023-04-27 20:08:19 -0300

    feat: adicionado handle para tratar erros de forma global

--- 2023/nodeJs/projeto-03/src/app.ts
+++ 2023/nodeJs/projeto-03/src/app.ts
@@ -1,6 +1,23 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation Error", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: aqui deveria ser feito um log para uma ferramente externa, tipo DataDog/NewRelic/Sentry
+  }
+  return reply.status(500).send({ message: "Internal Server Error." });
+});
