a3f7285f014a17961f92e43196d5ca89c5393d03
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2023-04-28 14:04:49 -0300

    feat: adicionado test de hash

--- 2023/nodeJs/projeto-03/src/use-cases/register.test.ts
+++ 2023/nodeJs/projeto-03/src/use-cases/register.test.ts
@@ -1,5 +1,37 @@
-import { expect, test } from "vitest";
+import { expect, describe, it } from "vitest";
+import { RegisterUseCase } from "./register";
+import { compare } from "bcryptjs";
+import { Prisma, User } from "@prisma/client";
 
-test("chegando se está funcionando", () => {
-  expect(1 + 1).toBe(2);
+describe("Register Use Case", () => {
+  it("a senha do usuario deve ser um hash quando for cadastrar", async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null;
+      },
+
+      async create(data: Prisma.UserCreateInput): Promise<User> {
+        const passwordHash = data.password_hash || "";
+        return {
+          id: "user-1",
+          name: data.name,
+          email: data.email,
+          password_hash: passwordHash,
+          created_at: new Date(),
+        };
+      },
+    });
+
+    const { user } = await registerUseCase.execute({
+      name: "Fulano",
+      email: "fulano@gmail.com",
+      password: "123456",
+    });
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
 });
--- 2023/nodeJs/projeto-03/src/use-cases/register.ts
+++ 2023/nodeJs/projeto-03/src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from "@/repositories/users-repository-interface";
 import { hash } from "bcryptjs";
 import { UserAlreadExistsError } from "./errors/user-already-exists-error";
+import { User } from "@prisma/client";
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -8,10 +9,18 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6); // 6 e o parametro para gerar o hash, zerar 6 rounds de complexidade
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -20,10 +29,12 @@ export class RegisterUseCase {
       throw new UserAlreadExistsError();
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+
+    return { user };
   }
 }
