a0f44b57966d711532b28ecf7a950219bfaff25d
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2023-05-01 16:46:35 -0300

    feat: adicionado testes dias diferentes

--- 2023/nodeJs/projeto-03/src/repositories/check-ins-repository.ts
+++ 2023/nodeJs/projeto-03/src/repositories/check-ins-repository.ts
@@ -2,4 +2,5 @@ import { CheckIN, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
   create(data: Prisma.CheckINUncheckedCreateInput): Promise<CheckIN>;
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIN | null>;
 }
