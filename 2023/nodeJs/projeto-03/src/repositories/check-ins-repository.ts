82502a475839d9434eabb02330eb0994aa372dc0
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2023-05-02 15:48:05 -0300

    feat: adicionado metodo listar check in do usuario

--- 2023/nodeJs/projeto-03/src/repositories/check-ins-repository.ts
+++ 2023/nodeJs/projeto-03/src/repositories/check-ins-repository.ts
@@ -2,5 +2,6 @@ import { CheckIN, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
   create(data: Prisma.CheckINUncheckedCreateInput): Promise<CheckIN>;
+  findManyByUserId(userId: string, page: number): Promise<CheckIN[]>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIN | null>;
 }
