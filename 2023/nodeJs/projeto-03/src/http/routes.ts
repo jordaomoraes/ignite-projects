7ae51c4d27b3f6cdf629f3a580842cec322f5a97
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2023-05-04 16:09:44 -0300

    feat: inicinado configuracao de JWT

--- 2023/nodeJs/projeto-03/src/http/routes.ts
+++ 2023/nodeJs/projeto-03/src/http/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register.controller";
+import { profile } from "@/http/controllers/profile.controller";
 import { authenticate } from "./controllers/authenticate.controller";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+  /** Authenticated */
+  app.get("/me", profile);
 }
