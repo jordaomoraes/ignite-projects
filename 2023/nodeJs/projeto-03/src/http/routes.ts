b36dc682c96e862f19988fcd7b434352c17e917f
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2023-05-05 14:13:35 -0300

    feat: adicionado middlewares Jwt

--- 2023/nodeJs/projeto-03/src/http/routes.ts
+++ 2023/nodeJs/projeto-03/src/http/routes.ts
@@ -2,10 +2,11 @@ import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register.controller";
 import { profile } from "@/http/controllers/profile.controller";
 import { authenticate } from "./controllers/authenticate.controller";
+import { verifyJWT } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
   /** Authenticated */
-  app.get("/me", profile);
+  app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
